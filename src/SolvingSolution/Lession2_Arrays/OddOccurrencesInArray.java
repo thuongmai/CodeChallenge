9f705fed1a3d60f1da38258b1ddffc15505a664d
Thuong Mai <thuongmai10292@gmail.com>
2018-07-04 15:51:14 -0400

    update lession 2 - better time complexity

--- src/SolvingSolution/Lession2_Arrays/OddOccurrencesInArray.java
+++ src/SolvingSolution/Lession2_Arrays/OddOccurrencesInArray.java
@@ -1,5 +1,8 @@
 package SolvingSolution.Lession2_Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Find the element in array
  * that don't have pairs.
@@ -12,7 +15,7 @@ package SolvingSolution.Lession2_Arrays;
 public class OddOccurrencesInArray {
 
 	//This take O(N^2) times
-	public static int solution (int[] A)
+	/*public static int solution (int[] A)
 	{
 		int count;
 		for (int i = 0; i < A.length; i++)
@@ -30,6 +33,28 @@ public class OddOccurrencesInArray {
 				return A[i];
 		}
 			
+		return 0;
+	}*/
+	
+	//total runtime is O(2n) or O(n), how about O(NlogN) ?
+	public static int solution (int[] A)
+	{
+		//Initialize an array to count element repetition
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		//O(n)
+		for (int i = 0; i < A.length; i++)
+		{
+			if (map.containsKey(A[i]))
+				map.put(A[i], map.get(A[i]) + 1);
+			else
+				map.put(A[i], 1);
+		}
+		//(O(n)
+		for (Integer key : map.keySet())
+		{
+			if (map.get(key) % 2 != 0)
+				return key;
+		}
 		return 0;
 	}
 	
